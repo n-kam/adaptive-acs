f606ac7c8332e3edc8ca146ab3b48ae807e1c0a5
n-kam <kamenshchiknv@mpei.ru>
2022-11-15 20:00:42 +0300

    UDP classes refactoring

--- UDPOut.py
+++ UDPOut.py
@@ -3,10 +3,11 @@ import struct
 
 
 class UDPOut(object):
-    def __init__(self, udp_ip, udp_port_out):
-        self.sock_rcv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock_rcv.bind((udp_ip, udp_port_out))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def __init__(self, ip, port):
+        self.sock.bind((ip, port))
 
     def rcv(self):
-        y_byte, addr = self.sock_rcv.recvfrom(8)
-        return struct.unpack('d', y_byte)[0]
+        byte_signal = self.sock.recvfrom(8)[0]
+        return struct.unpack('d', byte_signal)[0]
