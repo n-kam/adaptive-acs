5617a576613e762c0f86563234065bc53218e317
n-kam <kamenshchiknv@mpei.ru>
2022-11-28 02:20:49 +0300

    optimized TargetFunction class for speed

--- TargetFunction.py
+++ TargetFunction.py
@@ -3,7 +3,6 @@ import logging as log
 from control import matlab
 
 from ListIntegrator import ListIntegrator
-from TransferFunction import TransferFunction
 
 
 class TargetFunction(object):
@@ -15,6 +14,13 @@ class TargetFunction(object):
         self.nominator_len = nominator_len
         self.denominator_len = denominator_len
 
+        # Выделяем из списка "время / входная / выходная величина" только последний столбец
+        model_transient_response_list = list()
+        for i in range(len(self.model_transient_response)):
+            model_transient_response_list.append(self.model_transient_response[i][2])
+
+        self.model_transient_response_list = model_transient_response_list
+
     def tf(self, ab_values: list) -> float:
 
         if (self.nominator_len + self.denominator_len) != len(ab_values):
@@ -22,28 +28,17 @@ class TargetFunction(object):
 
         # Разделяем числитель и знаменатель из одного входного массива
         nominator = ab_values[:self.nominator_len]
-        log.debug("nominator: {}".format(nominator))
+        # log.debug("nominator: {}".format(nominator))
         denominator = ab_values[self.nominator_len:]
-        log.debug("denominator: {}".format(denominator))
+        # log.debug("denominator: {}".format(denominator))
 
         # Находим переходную характеристику для теоретической (подбираемой) функции с высокой частотой дискретизации
         matlab_transfer_function = matlab.tf(nominator, denominator)
-        timeline = [x / 1000 for x in range(0, 11000)]
-        # log.debug("timeline:{}".format(timeline))
+        # todo: переделать так, чтобы не было хардкода. список может получиться неправильной длины:
+        timeline = [x / 2 for x in range(0, 20)]
         [y, x] = matlab.step(matlab_transfer_function, timeline)
-        y = list(y)
-        x = list(x)
-        # log.debug("y: ".format(y))
+        optimization_transient_response_list = list(y)
+        # log.debug("Trans. resp. sizes should match. For model it is: {}; for optimizator: {}".format(
+        #     len(self.model_transient_response_list), len(y)))
 
-        # Выделяем из этой переходной характеристики только те временнЫе точки, которые имеются в модельной п.х.
-        # также собираем список чисто из значений модельной п.х. из списка "время / входной сигнал / выходной сигнал"
-        optimization_transient_response_list = list()
-        model_transient_response_list = list()
-        for i in range(len(self.model_transient_response)):
-            time_value = round(self.model_transient_response[i][0], 3)
-            index = x.index(time_value)
-            optimization_transient_response_list.append(y[index])
-            model_transient_response_list.append(self.model_transient_response[i][2])
-            # log.debug("time_value: {}".format(time_value))
-
-        return self.list_integrator.calc(model_transient_response_list, optimization_transient_response_list)
+        return self.list_integrator.calc(self.model_transient_response_list, optimization_transient_response_list)
