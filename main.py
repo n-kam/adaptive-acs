d40b0ca2dce6f81f228b29dc531bf37db6e43751
n-kam <kamenshchiknv@mpei.ru>
2022-11-28 01:41:29 +0300

    update model readings log

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ RUN_TIME = 10  # for how long to interact with model in sec
 # ШАГ 1. Идентификация объекта управления (его W(p))
 
 ooc_id = IdentifyOOC(UDP_IP_SEND, UDP_IP_OUT, UDP_PORT_IN, UDP_PORT_SET_POINT, UDP_PORT_OUT,
-                     source_file="h_model_1669571378.7674701.txt")
+                     source_file="h_model_1669583451.0397856.txt")
 w_ooc = ooc_id.identify()
 
 # STEP 2. SET UP PID-CONTROLLER
