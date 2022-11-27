e2b01a6318ab024819942cd251c171a388098569
n-kam <kamenshchiknv@mpei.ru>
2022-11-27 21:39:57 +0300

    make logging more verbose for Optimization

--- Optimization.py
+++ Optimization.py
@@ -16,26 +16,30 @@ class Optimization(object):
              beta1=0.9,
              beta2=0.999,
              max_iter=10000) -> list:
+        log.info("Started Adam optimization algorithm")
         values = numpy.array(values)
         M = numpy.array([0.0] * len(values))
         V = numpy.array([0.0] * len(values))
         values_prev = values + numpy.array([values_precision * 2] * len(values))
         iteration = 1
         time_start = time.time()
+        log.debug("Started Adam optimization algorithm while loop")
 
         while (abs((values - values_prev).any()) > values_precision) & (iteration < max_iter) & (
                 target_func(values) > tf_precision):
             values_prev = values
+            log.debug("Start gradient calculation")
             gradient = numdifftools.Gradient(target_func)(values)
+            log.debug("Finish gradient calculation")
             M = beta1 * M + (1 - beta1) * gradient
             V = beta2 * V + (1 - beta2) * gradient ** 2
             m = M / (1 - beta1 ** iteration)
             v = V / (1 - beta2 ** iteration)
             values = values - step * m / (numpy.sqrt(v) + values_precision)
             iteration += 1
-            print("tf: ", target_func(values), " [", iteration, "]: values=", values, "; grad=", gradient)
+            log.debug("tf={}; [{}] values={}; grad={}".format(target_func(values), iteration, values, gradient))
 
         time_end = time.time()
 
-        print("time of run: ", time_end - time_start)
+        log.info("time of run: {}".format(time_end - time_start))
         return list(values)
