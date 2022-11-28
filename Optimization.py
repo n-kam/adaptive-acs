38b29d9aa38d13a6e7661bba1bcd911b9f1c5680
n-kam <kamenshchiknv@mpei.ru>
2022-11-28 03:17:54 +0300

    optimize Optimizator for faster calculation. save best values in case we exceed max iter limit. change some coefficients again

--- Optimization.py
+++ Optimization.py
@@ -10,23 +10,28 @@ class Optimization(object):
 
     @staticmethod
     def adam(target_func, values: list[float],
-             values_precision=0.01,
+             values_precision=0.1,
              tf_precision=1e-6,
-             step=0.1,
+             # todo: подобрать более оптимальное значение шага, желательно, в зависимости от степеней входной функции:
+             step=0.01,
              beta1=0.9,
              beta2=0.999,
-             max_iter=10000) -> list:
+             max_iter=10000) -> list[float]:
         # log.debug("Started Adam optimization algorithm")
         values = numpy.array(values)
+        best_values = values
         M = numpy.array([0.0] * len(values))
         V = numpy.array([0.0] * len(values))
         values_prev = values + numpy.array([values_precision * 2] * len(values))
-        iteration = 1
+        iteration = 0
         time_start = time.time()
+        target_func_curr_value = target_func(values)
+        target_func_min_value = 1e10
         # log.debug("Started Adam optimization algorithm while loop")
 
         while (abs((values - values_prev).any()) > values_precision) & (iteration < max_iter) & (
-                target_func(values) > tf_precision):
+                target_func_curr_value > tf_precision):
+            iteration += 1
             values_prev = values
             # log.debug("Start gradient calculation")
             gradient = numdifftools.Gradient(target_func)(values)
@@ -36,10 +41,22 @@ class Optimization(object):
             m = M / (1 - beta1 ** iteration)
             v = V / (1 - beta2 ** iteration)
             values = values - step * m / (numpy.sqrt(v) + values_precision)
-            iteration += 1
+            target_func_curr_value = target_func(values)
+
+            # Запоминаем лучше значения коэффициентов на случай вылета за отсечку по итерациям, т.к. значение целевой
+            # функции может скакать в процессе расчета и последние значения в случае вылета могут не быть лучшими
+            if target_func_curr_value < target_func_min_value:
+                target_func_min_value = target_func_curr_value
+                best_values = values
+
             log.debug("tf={}; [{}] values={}; grad={}".format(target_func(values), iteration, values, gradient))
 
         time_end = time.time()
 
         log.info("time of run: {}".format(time_end - time_start))
-        return list(values)
+
+        if iteration == max_iter:
+            log.warning("Maximum number of iterations exceeded. Returned values may be suboptimal")
+            values = best_values
+
+        return values
