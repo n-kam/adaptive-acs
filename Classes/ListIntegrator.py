bc6b0ef4c8425aac20d53957ae3765e6e7656982
n-kam <kamenshchiknv@mpei.ru>
2022-12-03 13:48:47 +0300

    add enable exception for list sizes mismatch

--- Classes/ListIntegrator.py
+++ Classes/ListIntegrator.py
@@ -11,8 +11,8 @@ class ListIntegrator(object):
     def calc(list_1: list[float], list_2: list[float]) -> float:
         integral_sum = 0
 
-        # if len(list_2) != len(list_1):
-        #     raise Exception("List sizes mismatch")
+        if len(list_2) != len(list_1):
+            raise Exception("List sizes mismatch")
 
         for i in range(len(list_1)):
             integral_sum += abs(list_1[i] - list_2[i])
